26225d4ca975e83d3cb170355760373d1fc0e9e5
martha <meeney03@gmail.com>
2023-11-18 20:57:24 +0100

    basic solution

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
+from config import SECRET_KEY
 
+csrf = CSRFProtect()
 app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET_KEY
+csrf.init_app(app)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,22 +1,25 @@
-from flask import render_template
+from flask import render_template, redirect, flash
 from app import app
+from app.forms import MessageForm
 from openai import OpenAI
 
-@app.route("/")
-@app.route("/index")
+@app.route("/", methods=["GET", "POST"])
+@app.route("/index", methods=["GET", "POST"])
 def index():
-    client = OpenAI()
-    completion = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {
-                "role": "user",
-                "content": "Write a poem about web development.",
-            },
-        ],
-    )
-
-    return completion.choices[0].message.content
+    form = MessageForm()
+    if form.validate_on_submit():
+        client = OpenAI()
+        completion = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {
+                    "role": "user",
+                    "content": form.message.data,
+                },
+            ],
+        )
+        return render_template("index.html", form=form, answer=completion.choices[0].message.content)
+    return render_template("index.html", form=form)
 
 
 
